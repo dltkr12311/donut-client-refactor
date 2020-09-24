d9234728504e81491184663234cb2095a8be5893
billowycloud <bjkim0228@naver.com>
2020-09-24 17:21:21 +0900

    completed activity routing

--- src/Components/Common/Router.js
+++ src/Components/Common/Router.js
@@ -1,8 +1,9 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import IntroPage from "../../Pages/IntroPage";
-import ActivityPage from "../../Pages/ActivityPage";
-import MyListPage from "../../Pages/MyListPage";
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import IntroPage from '../../Pages/IntroPage';
+import ActivityPage from '../../Pages/ActivityPage';
+import MyListPage from '../../Pages/MyListPage';
+import MyPagePage from '../../Pages/MyPagePage';
 
 export default () => (
   <Router>
@@ -13,7 +14,8 @@ export default () => (
         <Route path="/signin" />
         <Route path="/signup" />
         <Route path="/mylist" component={MyListPage} />
-        <Route path="/mypage" />
+        <Route path="/mypage" component={MyPagePage} />
+        <Route path="/mypage/activity" />
         <Route path="/locate" />
         <Route path="/write" />
         <Redirect from="*" to="/" />
