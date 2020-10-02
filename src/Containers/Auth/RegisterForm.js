4d0a0be994b328855dffc198d2a68ee4bcfa4d50
billowycloud <bjkim0228@naver.com>
2020-10-02 16:40:09 +0900

    add latlon

--- src/Containers/Auth/RegisterForm.js
+++ src/Containers/Auth/RegisterForm.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { changeField, initializeForm, register } from "../../modules/auth";
-import AuthForm from "../../Components/Auth/AuthForm";
-import { check } from "../../modules/user";
-import { withRouter } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { changeField, initializeForm, register } from '../../modules/auth';
+import AuthForm from '../../Components/Auth/AuthForm';
+import { check } from '../../modules/user';
+import { withRouter } from 'react-router-dom';
 
 const RegisterForm = ({ history }) => {
   const [error, setError] = useState(null);
@@ -20,12 +20,14 @@ const RegisterForm = ({ history }) => {
   const onClick = (e) => {
     e.preventDefault();
     if (!navigator.geolocation) {
-      console.log("위치에러");
+      console.log('위치에러');
     } else {
       navigator.geolocation.getCurrentPosition((position) => {
-        let latitude = position.coords.latitude;
-        let longitude = position.coords.longitude;
+        const latitude = position.coords.latitude;
+        const longitude = position.coords.longitude;
+        const result = `${latitude},${longitude}`;
         document.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
+        dispatch(changeField({ form: 'register', key: 'latlon', value: result }));
         console.log(` 경도: ${latitude} 위도: ${longitude}`);
       });
     }
@@ -36,7 +38,7 @@ const RegisterForm = ({ history }) => {
     const { value, name } = e.target;
     dispatch(
       changeField({
-        form: "register",
+        form: 'register',
         key: name,
         value,
       })
@@ -46,40 +48,40 @@ const RegisterForm = ({ history }) => {
   //폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
-    const { email, password, passwordConfirm, username, address } = form;
+    const { email, password, passwordConfirm, username, latlon } = form;
     //하나라도 비어 있다면
-    if ([email, password, passwordConfirm, username].includes("")) {
-      setError("빈 칸을 모두 입력하세요");
+    if ([email, password, passwordConfirm, username].includes('')) {
+      setError('빈 칸을 모두 입력하세요');
       return;
     }
-    // if (address.includes("")) {
-    //   setError("위치가 설정되지 않았습니다");
-    //   return;
-    // }
+    if (latlon.length <= 0) {
+      setError('위치가 설정되지 않았습니다');
+      return;
+    }
     if (password !== passwordConfirm) {
-      setError("비밀번호가 일치하지 않습니다");
-      changeField({ form: "register", key: "password", value: "" });
-      changeField({ form: "register", key: "passwordConfirm", value: "" });
+      setError('비밀번호가 일치하지 않습니다');
+      changeField({ form: 'register', key: 'password', value: '' });
+      changeField({ form: 'register', key: 'passwordConfirm', value: '' });
       return;
     }
 
-    dispatch(register({ email, password, username, address }));
+    dispatch(register({ email, password, username, latlon }));
   };
 
   useEffect(() => {
-    dispatch(initializeForm("register"));
+    dispatch(initializeForm('register'));
   }, [dispatch]);
 
   //회원가입 성공/실패
   useEffect(() => {
     if (authError) {
       if (authError.response.status === 409) {
-        setError("이미 존재하는 계정입니다");
+        setError('이미 존재하는 계정입니다');
         return;
       }
     }
     if (auth) {
-      console.log("회원가입 완료:", auth);
+      console.log('회원가입 완료:', auth);
       dispatch(check());
     }
   }, [auth, authError, dispatch]);
