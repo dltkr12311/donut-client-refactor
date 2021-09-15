fa1c6875b48998598c54ebde2d913673f4f23d95
dltkr12311 <leesak94@gmail.com>
2021-09-15 16:08:32 +0900

    update: README.md 수정

--- src/lib/api/client.js
+++ src/lib/api/client.js
@@ -1,9 +1,9 @@
-import axios from 'axios';
+import axios from "axios";
 
 const client = axios.create();
 
 // API 주소를 다른 곳으로 사용함
-client.defaults.baseURL = 'http://localhost:4000';
+client.defaults.baseURL = "http://localhost:4000";
 // client.defaults.headers.common['authorization'] = `Bearer ${localStorage.getItem(
 //   'accessToken'
 // )}`;
